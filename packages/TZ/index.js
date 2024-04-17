7d0b28931d6a4e9c59635acfeffeb20c244502ee
Ting <2074683629@qq.com>
2024-04-17 16:44:01 +0800

    1

--- packages/TZ/index.js
+++ packages/TZ/index.js
@@ -13,9 +13,20 @@ Page({
     tabIndex: 0,
     bgList: ['#f89aa3', '#f7a5c0', '#f8b187', '#b8c1dd', '#f8e8ab', '#a5bbf0'], // 背景列表
     borderList: ['solid', 'dashed', 'dotted'], // 边框列表
-    stickerList: [`${url}/flower-1.png`, `${url}/flower-2.png`, `${url}/flower-3.png`, `${url}/flower-4.png`, `${url}/flower-5.png`, `${url}/flower-6.png`, `${url}/flower-7.png`, `${url}/flower-8.png` ] // 贴纸列表
+    stickerList: [`${url}/flower-1.png`, `${url}/flower-2.png`, `${url}/flower-3.png`, `${url}/flower-4.png`, `${url}/flower-5.png`, `${url}/flower-6.png`, `${url}/flower-7.png`, `${url}/flower-8.png`], // 贴纸列表
+    bgChecked: '', // 选中的背景
+    borderChecked: '',
+    stickerChecked: '',
   },
-  onLoad(options) {},
+  onLoad(options) {
+    this.initCanvas()
+  },
+  initCanvas(){
+    const canvasContext = wx.createCanvasContext('myCanvas');
+    canvasContext.setFillStyle(this.data.bgChecked)  // 绘制背景色
+    canvasContext.fillRect(10, 0, 350, 450);
+    canvasContext.draw();
+  },
   changeTab(e) {
     const {
       item,
@@ -24,6 +35,30 @@ Page({
     this.setData({
       tabIndex: index
     })
+  },
+  changeMaterial(e) {
+    const {
+      item,
+      type
+    } = e.currentTarget.dataset
+    switch (type) {
+      case 'bg': // 背景
+        this.setData({
+          bgChecked: item
+        })
+        break;
+      case 'border': // 边框
+        this.setData({
+          borderChecked: item
+        })
+        break;
+      case 'sticker': // 贴纸
+        this.setData({
+          stickerChecked: item
+        })
+        break;
+    }
+    this.initCanvas()
   },
   onShow() {},
   onShareAppMessage() {}
